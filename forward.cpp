d7b15d66ef3538a91d58d8e9c282681ea95beadd
Adam Polak <pl.adam.polak@gmail.com>
2013-12-19 14:12:30 +0100

    Optimize sequential algorithms

--- forward.cpp
+++ forward.cpp
@@ -11,13 +11,10 @@ namespace {
 template <class InputIterator1, class InputIterator2>
 int IntersectionSize(
     InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2, InputIterator2 last2,
-    int skip_ge) {
+    InputIterator2 first2, InputIterator2 last2) {
   int result = 0;
   while (first1 != last1 && first2 != last2) {
-    if (*first1 >= skip_ge || *first2 >= skip_ge)
-      break;
-   if (*first1 < *first2)
+    if (*first1 < *first2)
       ++first1;
     else if (*first1 > *first2)
       ++first2;
@@ -48,8 +45,7 @@ uint64_t Forward(const AdjList& graph) {
     const int s = deg[i].second;
     for (const int t : graph[s]) {
       if (perm[t] <= i) continue;
-      c += IntersectionSize(
-          A[s].begin(), A[s].end(), A[t].begin(), A[t].end(), n);
+      c += IntersectionSize(A[s].begin(), A[s].end(), A[t].begin(), A[t].end());
       A[t].push_back(i);
     }
   }
@@ -68,25 +64,27 @@ uint64_t CompactForward(const AdjList& graph) {
   vector<int> perm(n);
   for (int i = 0; i < n; ++i)
     perm[deg[i].second] = i;
-  
+ 
   AdjList sorted_graph(n);
   for (int i = 0; i < n; ++i) {
-    sorted_graph[perm[i]] = graph[i];
-  }
-  for (auto& neighbors : sorted_graph) {
-    for (int& neighbor : neighbors)
-      neighbor = perm[neighbor];
-    sort(neighbors.begin(), neighbors.end());
+    const vector<int>& src = graph[i];
+    const int s = perm[i];
+    vector<int>& dst = sorted_graph[s];
+    dst.reserve(src.size());  // this wastes some memory but saves a lot of time
+    for (int t : src) {
+      t = perm[t];
+      if (t < s)
+        dst.push_back(t);
+    }
+    sort(dst.begin(), dst.end());
   }
 
   uint64_t c = 0;
   for (int s = 0; s < n; ++s) {
     for (const int t : sorted_graph[s]) {
-      if (t <= s) continue;
       c += IntersectionSize(
           sorted_graph[s].begin(), sorted_graph[s].end(),
-          sorted_graph[t].begin(), sorted_graph[t].end(),
-          s);
+          sorted_graph[t].begin(), sorted_graph[t].end());
     }
   }
 
@@ -94,12 +92,18 @@ uint64_t CompactForward(const AdjList& graph) {
 }
 
 uint64_t CompactForwardWithPreproc(const Edges& unordered_edges) {
+  Timer timer;
+  
   const int n = NumVertices(unordered_edges);
-  Edges edges(unordered_edges.size());
+  Log() << "Calc num vertices " << timer.SinceLast();
+  
+  Edges edges;
   {
+    //*
     vector<int> deg(n);
     for (const pair<int, int>& edge : unordered_edges)
       deg[edge.first]++;
+    Log() << "Calc degs " << timer.SinceLast();
     vector<pair<int, int>> temp(n);
     for (int i = 0; i < n; ++i)
       temp[i] = make_pair(n - deg[i], i);
@@ -107,14 +111,20 @@ uint64_t CompactForwardWithPreproc(const Edges& unordered_edges) {
     vector<int> rename(n);
     for (int i = 0; i < n; ++i)
       rename[temp[i].second] = i;
-    for (int i = 0; i < unordered_edges.size(); ++i) {
-      edges[i].first = rename[unordered_edges[i].first];
-      edges[i].second = rename[unordered_edges[i].second];
+    Log() << "Calculate renaming permutation " << timer.SinceLast();
+    //*/
+    edges.reserve(unordered_edges.size() / 2);
+    for (const pair<int, int>& edge : unordered_edges) {
+      int s = edge.first, t = edge.second;
+      s = rename[s];
+      t = rename[t];
+      if (s > t)
+        edges.push_back(make_pair(s, t));
     }
+    Log() << "Copy and (optionally) rename edges " << timer.SinceLast();
     sort(edges.begin(), edges.end());
+    Log() << "Sort edges " << timer.SinceLast();
   }
-
-  Timer timer;
 
   vector<int> ends(edges.size());
   for (int i = 0; i < edges.size(); ++i)
@@ -122,20 +132,24 @@ uint64_t CompactForwardWithPreproc(const Edges& unordered_edges) {
 
   vector<vector<int>::iterator> pointers;
   pointers.reserve(n + 1);
-  pointers.push_back(ends.begin());
+  for (int k = 0; k <= edges[0].first; ++k)
+    pointers.push_back(ends.begin());
   for (int i = 1; i < edges.size(); ++i)
     if (edges[i-1].first != edges[i].first) {
-      assert(edges[i-1].first + 1 == edges[i].first);  // no isolated vertices
-      pointers.push_back(ends.begin() + i);
+      int k = edges[i].first - edges[i-1].first;
+      while (k--)
+        pointers.push_back(ends.begin() + i);
     }
-  pointers.push_back(ends.end());
-
+  while (pointers.size() < n + 1)
+    pointers.push_back(ends.end());
+  
+  Log() << "Total preproc " << timer.SinceStart();
+  
   uint64_t c = 0;
   for (const pair<int, int>& edge : edges) {
     const int s = edge.first, t = edge.second;
-    if (t <= s) continue;
     c += IntersectionSize(
-        pointers[s], pointers[s+1], pointers[t], pointers[t+1], s);
+        pointers[s], pointers[s+1], pointers[t], pointers[t+1]);
   }
   return c;
 }
