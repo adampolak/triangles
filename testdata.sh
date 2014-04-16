23bb79fdf0ada7c996386125ef363e7f874d86a7
Adam Polak <pl.adam.polak@gmail.com>
2014-04-16 18:20:01 +0200

    Testdata

---
+++ testdata.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TESTDATA="
+http://snap.stanford.edu/data/as-skitter.txt.gz
+http://snap.stanford.edu/data/bigdata/communities/com-lj.ungraph.txt.gz
+http://snap.stanford.edu/data/bigdata/communities/com-orkut.ungraph.txt.gz
+http://www.cc.gatech.edu/dimacs10/archive/data/coauthor/coPapersCiteseer.graph.bz2
+http://www.cc.gatech.edu/dimacs10/archive/data/coauthor/coPapersDBLP.graph.bz2
+http://www.cc.gatech.edu/dimacs10/archive/data/kronecker/kron_g500-logn16.graph.bz2
+http://www.cc.gatech.edu/dimacs10/archive/data/kronecker/kron_g500-logn17.graph.bz2
+http://www.cc.gatech.edu/dimacs10/archive/data/kronecker/kron_g500-logn18.graph.bz2
+http://www.cc.gatech.edu/dimacs10/archive/data/kronecker/kron_g500-logn19.graph.bz2
+http://www.cc.gatech.edu/dimacs10/archive/data/kronecker/kron_g500-logn20.graph.bz2
+"
+
+make convert-from-snap-main.e
+make convert-from-dimacs-main.e
+
+mkdir -p data
+cd data
+
+for url in $TESTDATA
+do
+  base=`echo $url | grep -o "[^/]\+$" | sed "s/\(\.txt\.gz\|\.graph\.bz2\)$//"`
+  if [ -f ${base}.bin ]
+  then
+    echo Skipping $base because it already exists
+    continue
+  fi
+  wget $url
+  echo Unzipping and converting
+  if [[ $url == *.txt.gz ]]
+  then
+    gzip -d ${base}.txt.gz
+    ../convert-from-snap-main.e ${base}.txt ${base}.bin
+    rm ${base}.txt
+  else
+    bzip2 -d ${base}.graph.bz2
+    ../convert-from-dimacs-main.e ${base}.graph ${base}.bin
+    rm ${base}.graph
+  fi
+done
